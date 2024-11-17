79f83e7533b875afe560db43d2fa0e8c3655f1e8
evtish <evevtish@gmail.com>
2024-11-17 16:58:12 +0300

    incorrect converting

--- file_helper.py
+++ file_helper.py
@@ -20,11 +20,9 @@ class FileConverter:
         with open(self.filename, 'rb') as bin_file:
             bin_file_data = bin_file.read()
 
-        # txt_file_data = bin_file_data.decode(encoding=lang_file_encoding, errors='ignore')
-        # txt_file_data = bin_file_data.hex()
-        # txt_file_data = bin_file_data.replace(' '.encode(), chr(0).encode())
+        txt_file_data = format_text(bin_file_data)
         with open(txt_filename + '.txt', 'wb') as txt_file:
-            txt_file.write(bin_file_data)
+            txt_file.write('\n'.encode(encoding=lang_file_encoding).join(txt_file_data))
 
     def txt_to_bin(self, bin_filename: str) -> None:
         with open(self.filename, 'rb') as txt_file:
@@ -41,6 +39,34 @@ class FileConverter:
 
 def get_file_extension(file: LangFileType) -> str:
     return path.splitext(file)[1][1:]
+
+
+def format_text(text: bytes) -> list[bytes]:
+    found_val = (' ' * 2).encode(encoding=lang_file_encoding)
+    res_list = []
+    # i = 0
+    # while text[i:].find(found_val) != -1:
+    #     pass
+
+    try:
+        cur_index = text.index(found_val)
+        new_start_index = cur_index + 2
+        additional_len = 0
+
+        while text[new_start_index] == ' ':
+            new_start_index += 1
+            additional_len += 1
+        additional_len //= 2
+
+        cur_str = text[:cur_index]
+        res_list.append(
+            cur_str + f'   Current length: {len(cur_str)}, max length: {len(cur_str) + additional_len}'
+            .encode(encoding=lang_file_encoding)
+        )
+        format_text(text[new_start_index:])
+
+    except ValueError:
+        return res_list
 
 
 # def get_binary_code(file_bytes: bytes) -> str:
