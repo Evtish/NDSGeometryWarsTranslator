16dbdaeb1461fc322919967db22a884a4710f534
evtish <evevtish@gmail.com>
2024-11-21 20:09:06 +0300

    fix converting

--- file_helper.py
+++ file_helper.py
@@ -10,40 +10,36 @@ from typing import TypeVar
 from text_formatter import TextFormatter
 
 LangFileType = TypeVar('LangFileType', PathLike[bytes],  PathLike[str],  str)
-lang_file_encoding = 'utf-8'
 
 
 class FileConverter:
     def __init__(self, filename: LangFileType) -> None:
-        self.filename = filename
+        self._filename: LangFileType = filename
+        # self._binary_file_data = self._read_binary_file()
+        # self._text_file_data = self._read_text_file()
+        self._byte_text_formatter: TextFormatter = TextFormatter(self._read_binary_file())
+
+    def _read_binary_file(self) -> bytes:
+        with open(self._filename, 'rb') as bin_file:
+            return bin_file.read()
+
+    def _read_text_file(self) -> str:
+        with open(self._filename, 'r') as txt_file:
+            return txt_file.read()
 
     def bin_to_txt(self, txt_filename: str) -> None:
-        with open(self.filename, 'rb') as bin_file:
-            bin_file_data = bin_file.read()
-
-        txt_file_data = format_text(bin_file_data)
-        with open(txt_filename + '.txt', 'wb') as txt_file:
-            txt_file.write('\n'.encode(encoding=lang_file_encoding).join(txt_file_data))
+        formatted_text_file_data = self._byte_text_formatter.get_formatted_text()
+        with open(txt_filename + '.txt', 'w') as txt_file:
+            txt_file.write(formatted_text_file_data)
 
     def txt_to_bin(self, bin_filename: str) -> None:
-        with open(self.filename, 'rb') as txt_file:
-            txt_file_data = txt_file.read()
-
-        # bin_file_data = bytes(txt_file_data).decode(encoding=lang_file_encoding, errors='ignore')
-        bin_file_data = txt_file_data.replace(
-            ' '.encode(encoding=lang_file_encoding),
-            chr(0).encode(encoding=lang_file_encoding)
-        )
+        formatted_binary_file_data = self._byte_text_formatter.get_reformatted_text()
         with open(bin_filename + '.bin', 'wb') as bin_file:
-            bin_file.write(bin_file_data)
+            bin_file.write(formatted_binary_file_data)
 
 
 def get_file_extension(file: LangFileType) -> str:
     return path.splitext(file)[1][1:]
-
-
-# def get_binary_code(file_bytes: bytes) -> str:
-#     return ''.join(str(file_bytes))[2:-1].replace('\\x00' * 2, '\n')
 
 
 def get_all_files() -> list[LangFileType]:
--- text_formatter.py
+++ text_formatter.py
@@ -1,3 +1,65 @@
+lang_file_encoding = 'latin-1'
+
+
+class TextFormatter:
+    def __init__(self, text_to_edit: bytes) -> None:
+        self._text_to_edit: bytes = text_to_edit
+        self._formatted_text_list: list[str]
+        self._line_lengths: tuple[int, int]
+        self._formatted_text_list, self._line_lengths = self._format_text()
+        self._reformatted_text_list: list[bytes] = self._reformat_text()
+
+    def _format_text(self) -> tuple[list[str], list[tuple[int, int]]]:
+        res_text_list, lengths_list = [], []
+        cur_line = ''
+        while (cur_index := self._text_to_edit.find(b'\x00')) != -1:
+            space_quantity = get_space_quantity(self._text_to_edit[cur_index + 1:])
+            if space_quantity <= 3:
+                remained_spaces = ''
+                if space_quantity > 1:
+                    remained_spaces = ' '
+                cur_line += self._text_to_edit[:cur_index].decode(lang_file_encoding) + remained_spaces
+
+            elif space_quantity > 3:
+                additional_len = (space_quantity - 3) // 2
+                cur_line += self._text_to_edit[:cur_index]
+                add_line(res_text_list, cur_line, additional_len)
+                lengths_list.append((len(cur_line), additional_len))
+                cur_line = ''
+
+            self._text_to_edit = self._text_to_edit[cur_index + space_quantity:]
+
+        # обработка последней строки, если в конце неё нет пробелов
+        if self._text_to_edit:
+            cur_line += self._text_to_edit
+            add_line(res_text_list, cur_line, 0)
+            lengths_list.append((len(cur_line), 0))
+        return res_text_list, lengths_list
+
+    def _reformat_text(self) -> list[bytes]:
+        res_text_list = []
+        for i, line in enumerate(self._formatted_text_list):
+            line = line[:line.rfind('(') - 1]
+            edited_line = (
+                line
+                .replace('', '\x00')
+                .lstrip('\x00')
+            )
+
+            original_len, extra_len = self._line_lengths[i]
+            empty_symbol_quantity = extra_len - len(line) + original_len
+
+            edited_line += '\x00' * (empty_symbol_quantity * 2 + 2)
+            res_text_list.append(edited_line.encode(encoding=lang_file_encoding))
+        return res_text_list
+
+    def get_formatted_text(self) -> str:
+        return '\n'.join(self._formatted_text_list)
+
+    def get_reformatted_text(self) -> bytes:
+        return b''.join(self._reformatted_text_list)
+
+
 def add_line(line_list: list[str], line: str, extra_len: int) -> None:
     line_list.append(
         line +
@@ -13,60 +75,3 @@ def get_space_quantity(line: str) -> int:
             break
         space_quantity += 1
     return space_quantity
-
-
-class TextFormatter:
-    def __init__(self, text_to_edit: str) -> None:
-        self.text_to_edit = text_to_edit
-        self.formatted_text_list, self.line_lengths = self._format_text()
-        self.reformatted_text_list = self._reformat_text()
-
-    def _format_text(self) -> tuple[list[str], list[tuple[int, int]]]:
-        res_text_list, lengths_list = [], []
-        cur_line = ''
-        while (cur_index := self.text_to_edit.find(' ')) != -1:
-            space_quantity = get_space_quantity(self.text_to_edit[cur_index + 1:])
-            if space_quantity <= 3:
-                remained_spaces = ''
-                if space_quantity > 1:
-                    remained_spaces = ' '
-                cur_line += self.text_to_edit[:cur_index] + remained_spaces
-
-            elif space_quantity > 3:
-                additional_len = (space_quantity - 3) // 2
-                cur_line += self.text_to_edit[:cur_index]
-                add_line(res_text_list, cur_line, additional_len)
-                lengths_list.append((len(cur_line), additional_len))
-                cur_line = ''
-
-            self.text_to_edit = self.text_to_edit[cur_index + space_quantity:]
-
-        # обработка последней строки, если в конце неё нет пробелов
-        if self.text_to_edit:
-            cur_line += self.text_to_edit
-            add_line(res_text_list, cur_line, 0)
-            lengths_list.append((len(cur_line), 0))
-        return res_text_list, lengths_list
-
-    def _reformat_text(self) -> list[str]:
-        res_text_list = []
-        for i, line in enumerate(self.formatted_text_list):
-            line = line[:line.rfind('(') - 1]
-            edited_line = (
-                line
-                .replace('', '\x00')
-                .lstrip('\x00')
-            )
-
-            original_len, extra_len = self.line_lengths[i]
-            empty_symbol_quantity = extra_len - len(line) + original_len
-
-            edited_line += '\x00' * (empty_symbol_quantity * 2 + 2)
-            res_text_list.append(edited_line)
-        return res_text_list
-
-    def get_formatted_text(self) -> str:
-        return '\n'.join(self.formatted_text_list)
-
-    def get_reformatted_text(self) -> str:
-        return ''.join(self.reformatted_text_list)
